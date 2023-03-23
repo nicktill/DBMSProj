4b478e12a97eb02ac5ec15361cdca86285dc79a5
Jonah Osband <79324007+josband@users.noreply.github.com>
2023-03-23 10:58:48 -0400

    Added .gitignore + base python script

---
+++ generate-data.py
@@ -0,0 +1,8 @@
+def main():
+    # Open our data insert file, can change later if needed
+    open('./data-insert.sql', 'w')
+
+    #TODO: write script to insert 100 users, 200 friendships, 10 groups, 300 messages
+
+if __name__ == '__main__':
+    main()
