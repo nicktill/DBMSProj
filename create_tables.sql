1dfdfe7f06acc1910a470c0cf2d6675a9c513792
Jonah Osband <79324007+josband@users.noreply.github.com>
2023-03-23 13:00:15 -0400

    Named constraints, formatted code and strings

--- create_tables.sql
+++ create_tables.sql
@@ -1,7 +1,9 @@
--------------------------------------------------
--- BeSocial Database Schema
--- Authors: Steven Jarmell, Jonah Osband, Nick Tillmann
--------------------------------------------------
+----------------------------------------------------------
+-- BeSocial Database Schema                             --
+-- Authors: Steven Jarmell, Jonah Osband, Nick Tillmann --
+----------------------------------------------------------
+
+-- NOTE: Assumptions are inline with our table schemas and triggers
 
 DROP TABLE IF EXISTS "user" CASCADE;
 DROP TABLE IF EXISTS friend CASCADE;
@@ -13,8 +15,6 @@ DROP TABLE IF EXISTS message CASCADE;
 DROP TABLE IF EXISTS messageRecipient CASCADE;
 DROP TABLE IF EXISTS Clock CASCADE;
 
--- NOTE: Assumptions are inline with our table schemas and triggers
-
 ------------------------------------------------
 -- TODO:
     -- Check about whether or not to have a NOT NULL on DOB for users
@@ -25,25 +25,29 @@ DROP TABLE IF EXISTS Clock CASCADE;
 
 -- Stores the user and login information for each user registered in the system.
 CREATE TABLE "user" (
-    userID INTEGER, -- primary key
-    name VARCHAR(50) NOT NULL, -- Cannot make a user without a name. Names can be repeated
-    email VARCHAR(50) NOT NULL, -- Will also be unique, but since userID is an integer it is a better PK
-    password VARCHAR(50) NOT NULL, -- Users need a password for security reasons
-    date_of_birth DATE,
-    lastlogin TIMESTAMP,
+    userID          INT, -- primary key
+    name            VARCHAR(50) NOT NULL, -- Cannot make a user without a name. Names can be repeated
+    email           VARCHAR(50) NOT NULL, -- Will also be unique, but since userID is an integer it is a better PK
+    password        VARCHAR(50) NOT NULL, -- Users need a password for security reasons
+    date_of_birth   DATE,
+    lastlogin       TIMESTAMP,
+
+    -- Constraints
     CONSTRAINT PK_user PRIMARY KEY (userID),
     CONSTRAINT UQ_email UNIQUE (email),
-    CONSTRAINT IC_email_proper_form CHECK (email LIKE '%@%.%') -- email needs to follow this form
+    CONSTRAINT IC_email_proper_form CHECK ( email LIKE '%@%.%' ) -- email needs to follow this form
 );
 
 
 -- Stores the friends lists for every user in the system.
 -- The JDate is when they became friends, and the requestText is the text from the original friend request.
 CREATE TABLE friend (
-    userID1 INTEGER,
-    userID2 INTEGER,
-    JDate DATE NOT NULL,
-    requestText VARCHAR(200) DEFAULT 'Hey! Lets be friends.', -- Come back to later
+    userID1         INT,
+    userID2         INT,
+    JDate           DATE NOT NULL,
+    requestText     VARCHAR(200) DEFAULT E'Hey! Let\'s be friends.', -- Come back to later
+
+    -- Constraints
     CONSTRAINT PK_friend PRIMARY KEY (userID1, userID2),
     -- When a user is removed from the system, we should remove all dependent friendship entries.
     CONSTRAINT FK1_friend FOREIGN KEY (userID1) REFERENCES "user"(userID)
@@ -54,9 +58,11 @@ CREATE TABLE friend (
 
 -- Stores pending friends requests that have yet to be confirmed by the recipient of the request.
 CREATE TABLE pendingFriend (
-    fromID INTEGER,
-    toID INTEGER,
-    requestText VARCHAR(200) DEFAULT 'Hey! Lets be friends.',
+    fromID          INT,
+    toID            INT,
+    requestText     VARCHAR(200) DEFAULT E'Hey! Let\'s be friends.',
+
+    -- Constraints
     CONSTRAINT PK_pendingFriend PRIMARY KEY (fromID, toID),
     -- If a user is removed, we want to remove pending requests
     -- Same principle for updates
@@ -68,10 +74,12 @@ CREATE TABLE pendingFriend (
 
 -- Stores information for each group in the system.
 CREATE TABLE groupInfo (
-    gID INTEGER,
-    name VARCHAR(50) NOT NULL, -- Groups need to have a name associated with them
-    size INTEGER DEFAULT 1, -- When a group is created, the owner is the first member
-    description VARCHAR(200), -- Group descriptions can be null
+    gID             INT,
+    name            VARCHAR(50) NOT NULL, -- Groups need to have a name associated with them
+    size            INT DEFAULT 1, -- When a group is created, the owner is the first member
+    description     VARCHAR(200), -- Group descriptions can be null
+
+    -- Constraints
     CONSTRAINT PK_groupInfo PRIMARY KEY (gID),
     -- A group must always have at least one member
     CONSTRAINT IC_groupSize CHECK (size >= 1)
@@ -81,11 +89,12 @@ CREATE TABLE groupInfo (
 -- The "role" indicates whether a user is a manager of a group (who can accept joining group requests) or a member.
 -- The lastConfirmed attribute stores when the group member was successfully added to the group.
 CREATE TABLE groupMember (
-    gID INTEGER,
-    userID INTEGER,
-    role VARCHAR(20)
-        CHECK (role IN ('manager', 'member')),
-    lastConfirmed TIMESTAMP NOT NULL, -- The system should never have a NULL value for this
+    gID             INT,
+    userID          INT,
+    role            VARCHAR(20),
+    lastConfirmed   TIMESTAMP NOT NULL, -- The system should never have a NULL value for this
+
+    -- Constraints
     CONSTRAINT PK_groupMember PRIMARY KEY (gID, userID),
     -- When a group is deleted, the group member entries should be deleted since
     -- since the group no longer exists
@@ -93,16 +102,20 @@ CREATE TABLE groupMember (
         ON DELETE CASCADE ON UPDATE CASCADE,
     -- If a user is deleted, all of their respective groupMember entries should be deleted
     CONSTRAINT FK2_groupMember FOREIGN KEY (userID) REFERENCES "user"(userID)
-        ON DELETE CASCADE ON UPDATE CASCADE
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    -- The DB manager can have varying roles
+    CONSTRAINT IC_group_role CHECK ( role IN ( 'manager', 'member' ) )
 );
 
 -- Stores pending joining group requests that have yet to be accepted/rejected by the manager of the group.
 -- The requestTime is the time when the user requested group membership.
 CREATE TABLE pendingGroupMember (
-    gID INTEGER,
-    userID INTEGER,
-    requestText VARCHAR(200) DEFAULT 'I would like to join your group! :)',
-    requestTime TIMESTAMP,
+    gID             INT,
+    userID          INT,
+    requestText     VARCHAR(200) DEFAULT 'I would like to join your group! :)',
+    requestTime     TIMESTAMP,
+
+    -- Constraints
     CONSTRAINT PK_pendingGroupMember PRIMARY KEY (gID, userID),
     -- When a group is deleted, the pending group member entries should be deleted since
     -- since the group no longer exists
@@ -116,12 +129,14 @@ CREATE TABLE pendingGroupMember (
 -- Stores every message sent by users in the system.
 -- Note that the default values of toUserID and toGroupID should be NULL.
 CREATE TABLE message (
-    msgID INTEGER,
-    fromID INTEGER,
-    messageBody VARCHAR(200) NOT NULL,
-    toUserID INTEGER DEFAULT NULL,
-    toGroupID INTEGER DEFAULT NULL,
-    timeSent TIMESTAMP,
+    msgID           INT,
+    fromID          INT,
+    messageBody     VARCHAR(200) NOT NULL,
+    toUserID        INT DEFAULT NULL,
+    toGroupID       INT DEFAULT NULL,
+    timeSent        TIMESTAMP,
+
+    -- Constraints
     CONSTRAINT PK_message PRIMARY KEY (msgID),
     -- If a sender is deleted, we should set the foreign key to null so that
     -- messages can be preserved for other users
@@ -137,8 +152,10 @@ CREATE TABLE message (
 
 -- Stores the recipients of each message stored in the system.
 CREATE TABLE messageRecipient (
-    msgID INTEGER,
-    userID INTEGER,
+    msgID           INT,
+    userID          INT,
+
+    -- Constraints
     CONSTRAINT PK_messageRecipient PRIMARY KEY (msgID, userID),
     -- If a message is deleted, this entry should be deleted
     CONSTRAINT FK1_messageRecipient FOREIGN KEY (msgID) REFERENCES message(msgID)
@@ -150,11 +167,13 @@ CREATE TABLE messageRecipient (
 );
 
 -- To facilitate time travel, you are expected to implement a Clock.
--- You must maintain a “pseudo” timestamp (not the real system timestamp) in the auxilliary table Clock.
+-- You must maintain a “pseudo” timestamp (not the real system timestamp) in the auxiliary table Clock.
 -- The reason for making such a timestamp and not using the system one is to make it easy to generate scenarios (time traveling)
 -- That is, all functions on the Clock relation will be done on the database side and not through JDBC.
 CREATE TABLE Clock (
-    pseudo_time TIMESTAMP,
+    pseudo_time     TIMESTAMP,
+
+    -- Constraints
     CONSTRAINT PK_Clock PRIMARY KEY (pseudo_time)
 );
 
