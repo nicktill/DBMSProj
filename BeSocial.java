bb66bf63674d64c157b69a62fe38204bbdf7c242
Jonah Osband <79324007+josband@users.noreply.github.com>
2023-04-10 17:53:56 -0400

    Resolved Java Compilation

--- BeSocial.java
+++ BeSocial.java
@@ -1,5 +1,3 @@
-package Phase2;
-
 import java.util.Scanner;
 
 import java.sql.*;
--- Driver.java
+++ Driver.java
@@ -1,4 +1,3 @@
-package Phase2;
 public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello World");
