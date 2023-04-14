dc3cf38d6bc7b41b72bfe0631f3ce290fd9cc1eb
Steven-Jarmell <sjj27@pitt.edu>
2023-04-13 21:38:28 -0400

    Add more descriptive error messages

--- BeSocial.java
+++ BeSocial.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+
 import org.postgresql.util.PSQLException;
 
 // **NOTE** PLEASE USE THE EXTENSION 'BetterNotes' to make this file more readable! **NOTE** 
@@ -594,7 +592,18 @@ public class BeSocial {
             callableStatement.execute();
             System.out.println("Successfully requested to join.");
         } catch (SQLException e) {
-            printErrors(e);
+            String message = e.getMessage();
+            while ((e = e.getNextException()) != null) {
+                message.concat(e.getMessage());
+            }
+
+            if (message.contains("violates foreign key constraint")) {
+                System.out.println("The group you tried to join does not exist");
+            } else if (message.contains ("already exists")) {
+                System.out.println("You already tried to join this group");
+            } else {
+                printErrors(e);
+            }
         }
     }
 
